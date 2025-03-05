7c0b3b743447cabf1d7220adbc7c081ab106db39
dmtrots <dmitrytrotss@gmail.com>
2025-03-05 22:13:31 +0500

    3. Утверждения с примитивными типами [#504883]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -21,42 +21,42 @@ class BoxTest {
     }
 
     @Test
-    void NUmberOfVerticesIsFour() {
+    void numberOfVerticesIsFour() {
         Box box = new Box(4, 6);
         int vertex = box.getNumberOfVertices();
         assertThat(vertex).isEqualTo(4);
     }
 
     @Test
-    void NUmberOfVerticesIsFive() {
+    void numberOfVerticesIsFive() {
         Box box = new Box(5, 7);
         int vertex = box.getNumberOfVertices();
         assertThat(vertex).isEqualTo(-1);
     }
 
     @Test
-    void FigureExists() {
+    void figureExists() {
         Box box = new Box(8, 12);
         boolean figure = box.isExist();
         assertThat(figure).isTrue();
     }
 
     @Test
-    void FigureDoesNotExist() {
+    void figureDoesNotExist() {
         Box box = new Box(5, 10);
         boolean figure = box.isExist();
         assertThat(figure).isFalse();
     }
 
     @Test
-    void AreaOfSphere() {
+    void areaOfSphere() {
         Box box = new Box(0, 4);
         double square = box.getArea();
         assertThat(square).isCloseTo(201.06d, withPrecision(0.01d));
     }
 
     @Test
-    void AreaOfTetrahedron() {
+    void areaOfTetrahedron() {
         Box box = new Box(4, 2);
         double square = box.getArea();
         assertThat(square).isCloseTo(7d, withPrecision(0.1d));
