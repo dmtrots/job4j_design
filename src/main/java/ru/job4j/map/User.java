5e102baf8a6b545615dd5a6e634313cb935721c4
dmtrots <dmitrytrotss@gmail.com>
2025-06-03 00:53:12 +0500

    7. Переопределить и equals и hashCode [#1002]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -14,6 +14,11 @@ public class User {
     }
 
     @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
