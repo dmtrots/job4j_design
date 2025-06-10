aa7efdde8cfbebcfe1a767a5e4237ab8ec5d2e34
dmtrots <dmitrytrotss@gmail.com>
2025-06-10 19:16:25 +0500

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,15 +9,33 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
-        search(start, path -> path.toFile().getName().startsWith("Simple") && path.toFile().getName().endsWith(".java"))
-                .forEach(System.out::println);
+        argsValidation(args);
+        Path start = Paths.get(args[0]);
+        String extension = args[1];
+        List<Path> result = search(start, path -> path.toFile().getName().endsWith(extension));
+        result.forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void argsValidation(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Количество передаваемых параметров не равно двум");
+        }
+        Path root = Paths.get(args[0]);
+        if (!root.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Директория не существует: %s", root.toAbsolutePath()));
+        }
+        if (!root.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Это не директория: %s", root.toAbsolutePath()));
+        }
+        String extension = args[1];
+        if (!extension.startsWith(".")) {
+            throw new IllegalArgumentException("Имя расширения не начинается с точки");
+        }
     }
 }
