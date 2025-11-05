576f603a1e42f6657b108a70c7ad00324a940d76
dmtrots <dmitrytrotss@gmail.com>
2025-11-05 16:12:56 +0500

    5. Log4j. FileAppender и другие appenders. [#504876 #554073]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,11 +1,11 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
@@ -13,5 +13,24 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        byte b = 10;
+        LOG.debug("b : {}", b);
+        short s = 200;
+        LOG.debug("s : {}", b);
+        int i = 1000;
+        LOG.debug("i : {}", b);
+        long l = 10000L;
+        LOG.debug("l : {}", b);
+        float f = 3.14F;
+        LOG.debug("f : {}", b);
+        double d = 2.74562;
+        LOG.debug("d : {}", b);
+        char c = 'A';
+        LOG.debug("c : {}", b);
+        boolean bool = true;
+        LOG.debug("bool : {}", b);
     }
 }
